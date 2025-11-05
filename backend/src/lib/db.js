5504d570f459c442a0121a1bf46e394e394d4d6d
PY-SENSEI <gauravjena48@gmail.com>
2025-11-05 15:50:55 +0530

    validation check added for db

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -4,6 +4,9 @@ import { ENV } from "./env.js";
 
 export const connectDB = async () => {
   try {
+    if (!ENV.DB_URL) {
+      throw new Error("Missing DB_URL environment variable");
+    }
     const conn = await mongoose.connect(ENV.DB_URL);
     console.log("Connected to mongodb", conn.connection.host);
   } catch (error) {
