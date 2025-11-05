87ce43152771acd0acc4d4da4f10a73c4b5d1a12
PY-SENSEI <gauravjena48@gmail.com>
2025-11-05 15:40:55 +0530

    db and tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,28 +1,37 @@
 import express from "express";
 import path from "path";
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 
 const __dirname = path.resolve();
 
 app.get("/Health", (req, res) => {
-    res.send("Website is up and running!!")
-})
+  res.send("Website is up and running!!");
+});
 app.get("/careers", (req, res) => {
-    res.send("Website is deployed!!")
-})
+  res.send("Website is deployed!!");
+});
 
 // make the app ready for the dployment
 if (ENV.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
- 
-    app.get("/{*any}", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-    })
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () =>
+      console.log("Server running on port: ", ENV.PORT)
+    );
+  } catch (error) {
+    console.log("Error starting the server", error);
+  }
+};
 
-app.listen(ENV.PORT, () => {
-    console.log("Server running on port: ", ENV.PORT )
-})
+startServer();
