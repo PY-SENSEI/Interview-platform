7472f3c21084330d0eaf2a593ac21567b7f873ae
PY-SENSEI <gauravjena48@gmail.com>
2025-11-05 12:55:55 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,27 @@
 import express from "express";
+import path from "path";
 import { ENV } from "./lib/env.js";
 
 const app = express();
 
+const __dirname = path.resolve();
+
 app.get("/Health", (req, res) => {
     res.send("Website is up and running!!")
 })
+app.get("/careers", (req, res) => {
+    res.send("Website is deployed!!")
+})
+
+// make the app ready for the dployment
+if (ENV.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+ 
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
+
 
 app.listen(ENV.PORT, () => {
     console.log("Server running on port: ", ENV.PORT )
